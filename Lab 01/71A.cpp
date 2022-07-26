b564c83b427471268aceed979708b4375b3cc728
gLaaSSb00m <109515301+gLaaSSb00m@users.noreply.github.com>
2022-07-26 21:48:24 +0600

    Add files via upload

---
+++ Lab 01/71A.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    while(n--)
+    {
+     string a;
+    cin>>a;
+    int num=a.size();
+    num>10 ? cout<<a[0]<<num-2<<a[num-1]<<endl:cout<<a<<endl;
+
+    }
+    return 0;
+
+
+}
