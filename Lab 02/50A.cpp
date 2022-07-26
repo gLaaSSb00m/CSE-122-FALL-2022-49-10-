928a8e91002385cac7c1f29697ac8d0fd28dab36
gLaaSSb00m <109515301+gLaaSSb00m@users.noreply.github.com>
2022-07-26 21:59:03 +0600

    Add files via upload

---
+++ Lab 02/50A.cpp
@@ -0,0 +1,13 @@
+//Codeforces Problem No - 50A
+//Problame name - Domino Pilling
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    //cin>>a>>b;
+    cout<<(a*b)/2<<endl;
+    return 0;
+}
